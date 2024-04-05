29f21ca4122428ea511aefa2f2093aeec410f863
NGR-Np <ngr112001@gmail.com>
2024-04-05 20:43:04 +0545

    cc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import MettingList from '@/section/MettingLists';
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
